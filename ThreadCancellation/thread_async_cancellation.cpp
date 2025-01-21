5b6ac7497840db3c4b3bff2ab49187053435cba4
Nitin Bargaje <nitinbargaje7@gmail.com>
2025-01-21 16:15:50 +0530

    Asynchronus and deffered cancellation of thread

--- ThreadCancellation/thread_async_cancellation.cpp
+++ ThreadCancellation/thread_async_cancellation.cpp
@@ -8,6 +8,16 @@
 
 pthread_t slaves[N_SLAVES];
 
+void memory_cleanup_handler(void *arg) {
+    printf("%s invoked....\n", __FUNCTION__);
+    free(arg);
+}
+
+void file_cleanup_handler(void *arg) {
+    printf("%s invoked....\n", __FUNCTION__);
+    fclose((FILE *)arg);
+}
+
 void * write_into_file(void *arg) {
     char file_name[64];
     char string_to_write[64];
@@ -21,15 +31,20 @@ void * write_into_file(void *arg) {
     pthread_setcanceltype(PTHREAD_CANCEL_ASYNCHRONOUS, 0);
 
     int *thread_id = (int *)arg;
+
+    pthread_cleanup_push(memory_cleanup_handler, arg);
+
     sprintf(file_name, "thread_%d.txt", *thread_id);
 
     FILE *fptr = fopen(file_name, "w");
 
     if(!fptr) {
         printf("Error: Could not open log file %s, errno = %d\n", file_name, errno);
-        return 0;
+        pthread_exit(0);
     }
 
+    pthread_cleanup_push(file_cleanup_handler, fptr);
+    
     while(1) {
         len = sprintf(string_to_write, "%d:\t I am thread %d\n", count++, *thread_id);
         fwrite(string_to_write, sizeof(char), len, fptr);
@@ -37,6 +52,8 @@ void * write_into_file(void *arg) {
         sleep(1);
     }
 
+    pthread_cleanup_pop(1);
+    pthread_cleanup_pop(1);
     return 0;
 }
 
